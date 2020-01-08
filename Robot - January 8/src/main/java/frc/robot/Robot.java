817c24d01efbb1bf058732e0bd451f0871830dcf
KyleDerbabian <kylederbabian@lhsla.org>
2020-01-08 14:48:03 -0800

    Update Robot.java

--- Robot - January 8/src/main/java/frc/robot/Robot.java
+++ Robot - January 8/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
@@ -34,7 +36,13 @@ public class Robot extends TimedRobot {
   CANSparkMax FR = new CANSparkMax(2, MotorType.kBrushless);
   CANSparkMax BR = new CANSparkMax(3, MotorType.kBrushless);
   CANEncoder encoder = FL.getEncoder(); 
-
+  I2C.Port i2cPort = I2C.Port.kOnboard;
+  ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  ColorMatch colorMatcher = new ColorMatch(); 
+  final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   /**
    * This function is run when the robot is first started up and should be
@@ -42,6 +50,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    colorMatcher.addColorMatch(kBlueTarget);
+    colorMatcher.addColorMatch(kGreenTarget);
+    colorMatcher.addColorMatch(kRedTarget);
+    colorMatcher.addColorMatch(kYellowTarget); 
   }
 
   /**
@@ -70,7 +82,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-
+    
     FL.set(joystick.getRawAxis(1));
     BL.set(joystick.getRawAxis(1));
     FR.set(joystick.getRawAxis(5));
@@ -79,6 +91,27 @@ public class Robot extends TimedRobot {
     encoderVelocity = encoder.getVelocity(); // returns RPM
     SmartDashboard.putNumber("Encoder Position", encoderPosition);
     SmartDashboard.putNumber("Encoder Velocity", encoderVelocity);
+
+    Color detectedColor = colorSensor.getColor();
+    String colorString;
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+
   }
 
   /**
