0647f6756e60a0353fa40756c5efaba9aa0c5de1
KyleDerbabian <kylederbabian@lhsla.org>
2020-01-18 17:06:28 -0800

    Update Robot.java

--- Robot - January 8/src/main/java/frc/robot/Robot.java
+++ Robot - January 8/src/main/java/frc/robot/Robot.java
@@ -7,59 +7,40 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.revrobotics.ColorSensorV3;
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorMatchResult;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.AnalogInput;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+
 public class Robot extends TimedRobot {
-
-  double encoderPosition;
-  double encoderVelocity;
-  Joystick joystick = new Joystick(0); 
-  TalonSRX FL = new TalonSRX(0);
-  TalonSRX BL = new TalonSRX(1);
-  TalonSRX FR = new TalonSRX(2);
-  TalonSRX BR = new TalonSRX(3);
-  //CANSparkMax FL = new CANSparkMax(0, MotorType.kBrushless);  
-  //CANSparkMax BL = new CANSparkMax(1, MotorType.kBrushless);
-  //CANSparkMax FR = new CANSparkMax(2, MotorType.kBrushless);
-  //CANSparkMax BR = new CANSparkMax(3, MotorType.kBrushless);
-  //CANEncoder encoder = FL.getEncoder(); 
+  Joystick joystick = new Joystick(0);
+  TalonSRX spinner = new TalonSRX(5);
+  CANSparkMax FL = new CANSparkMax(8, MotorType.kBrushless);
+  CANSparkMax BL = new CANSparkMax(9, MotorType.kBrushless);
+  CANSparkMax FR = new CANSparkMax(10, MotorType.kBrushless);
+  CANSparkMax BR = new CANSparkMax(11, MotorType.kBrushless);
+  CANEncoder encoder = FL.getEncoder();
   I2C.Port i2cPort = I2C.Port.kOnboard;
   ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
   ColorMatch colorMatcher = new ColorMatch(); 
+  String wantedColor;
+  String chosenColor;
   final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
   final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
   final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-  final double kHoldDistance = 12.0; // In inches
-  final double kValueToInches = 0.125;
-  final double kP = 0.05;
-  final int kUltrasonicPort = 0;
-  AnalogInput ultrasonic = new AnalogInput(kUltrasonicPort);
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
   @Override
   public void robotInit() {
     colorMatcher.addColorMatch(kBlueTarget);
@@ -68,53 +49,49 @@ public class Robot extends TimedRobot {
     colorMatcher.addColorMatch(kYellowTarget); 
   }
 
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
   @Override
   public void autonomousInit() {
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
   }
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
   @Override
   public void teleopInit() {
   }
 
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
   @Override
   public void teleopPeriodic() {
+    if (joystick.getRawButton(3)) {
+      FL.restoreFactoryDefaults();
+    }
+    SmartDashboard.putNumber("Encoder Position", encoder.getPosition());
+    SmartDashboard.putNumber("Encoder Velocity", encoder.getVelocity());
 
-    //encoderPosition = encoder.getPosition(); // returns position based on revolution
-    //encoderVelocity = encoder.getVelocity(); // returns RPM
-    //SmartDashboard.putNumber("Encoder Position", encoderPosition);
-    //SmartDashboard.putNumber("Encoder Velocity", encoderVelocity);
+    if (joystick.getRawButton(5)) {
+      encoder.setPosition(5);
+    }
 
+    FL.set(joystick.getRawAxis(1));
+
+    //spinner.set(ControlMode.PercentOutput, joystick.getRawAxis(1) / 4);
     Color detectedColor = colorSensor.getColor();
     String colorString;
     ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
 
     if (match.color == kBlueTarget) {
-      colorString = "Blue";
+      colorString = "B";
     } else if (match.color == kRedTarget) {
-      colorString = "Red";
+      colorString = "R";
     } else if (match.color == kGreenTarget) {
-      colorString = "Green";
+      colorString = "G";
     } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
+      colorString = "Y";
     } else {
-      colorString = "Unknown";
+      colorString = "U";
     }
+
 
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
@@ -122,28 +99,99 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Confidence", match.confidence);
     SmartDashboard.putString("Detected Color", colorString);
 
-    double currentDistance = ultrasonic.getValue() * kValueToInches;
-    double currentSpeed = (kHoldDistance - currentDistance) * kP;
+    // TEST CASE!!!! - chosen color = red
+    chosenColor = "G";
 
-    if (joystick.getRawButton(1)) {
-      FL.set(ControlMode.PercentOutput, currentSpeed / 4);
-      BL.set(ControlMode.PercentOutput, currentSpeed / 4);
-      FR.set(ControlMode.PercentOutput, currentSpeed / 4);
-      BR.set(ControlMode.PercentOutput, currentSpeed / 4);
-      SmartDashboard.putNumber("Current Distance", currentDistance);
-      SmartDashboard.putNumber("Current Speed", currentSpeed);
-    } else {
-      FL.set(ControlMode.PercentOutput, joystick.getRawAxis(1));
-      BL.set(ControlMode.PercentOutput, joystick.getRawAxis(1));
-      FR.set(ControlMode.PercentOutput, joystick.getRawAxis(5));
-      BR.set(ControlMode.PercentOutput, joystick.getRawAxis(5));
+    // Color Wheel II
+    if (joystick.getRawButton(4)) {
+      //Code for Color Wheel Part II (Color Picker)
+      if (chosenColor.equals("B")) {
+        wantedColor = "R";
+      } else if (chosenColor.equals("R")) {
+        wantedColor = "B";
+      } else if (chosenColor.equals("G")) {
+        wantedColor = "Y";
+      } else if (chosenColor.equals("Y")) {
+        wantedColor = "G";
+      }
+
+      spinner.set(ControlMode.PercentOutput, 0.8);
+      if (colorString.equals(wantedColor)) {
+        spinner.set(ControlMode.PercentOutput, 0);
+      }
+
     }
 
+    if (joystick.getRawButton(3)) {
+      spinner.set(ControlMode.PercentOutput, 0);
+    }
+
+    if (joystick.getRawButton(2)) {
+
+      for (int i = 0; i < 16; i++) {
+
+        //spinner.set(ControlMode.PercentOutput, 0);
+        DriverStation.reportWarning("Start of Loop", true);
+        DriverStation.reportWarning(match.color.toString(), true);
+
+        Color thisIsAColor = colorSensor.getColor();
+        ColorMatchResult thisIsAColorMatch = colorMatcher.matchClosestColor(thisIsAColor);
+
+        // breaker
+        if (joystick.getRawButton(1)) {
+          spinner.set(ControlMode.PercentOutput, 0);
+          break;
+        }
+
+        if (thisIsAColorMatch.color == kRedTarget) {
+
+          DriverStation.reportWarning(match.color.toString(), true);
+          Color loopColor1;
+          ColorMatchResult loopColorMatch1 = thisIsAColorMatch;
+
+          while (loopColorMatch1.color != kBlueTarget) {
+            loopColor1 = colorSensor.getColor();
+            loopColorMatch1 = colorMatcher.matchClosestColor(loopColor1);
+            spinner.set(ControlMode.PercentOutput, 0.5);
+            if (joystick.getRawButton(1)) {
+              spinner.set(ControlMode.PercentOutput, 0);
+              break;
+            }
+          }
+        } else if (thisIsAColorMatch.color == kBlueTarget) {
+
+          DriverStation.reportWarning(match.color.toString(), true);
+          Color loopColor2;
+          ColorMatchResult loopColorMatch2 = thisIsAColorMatch;
+
+          while (loopColorMatch2.color != kRedTarget) {
+            loopColor2 = colorSensor.getColor();
+            loopColorMatch2 = colorMatcher.matchClosestColor(loopColor2);
+            spinner.set(ControlMode.PercentOutput, 0.5);
+            if (joystick.getRawButton(1)) {
+              spinner.set(ControlMode.PercentOutput, 0);
+              break;
+            }
+          }
+        } else {
+          DriverStation.reportWarning(match.color.toString(), true);
+          Color loopColor3;
+          ColorMatchResult loopColorMatch3 = thisIsAColorMatch;
+
+          while (loopColorMatch3.color != kRedTarget) {
+            loopColor3 = colorSensor.getColor();
+            loopColorMatch3 = colorMatcher.matchClosestColor(loopColor3);
+            spinner.set(ControlMode.PercentOutput, 0.5);
+            if (joystick.getRawButton(1)) {
+              spinner.set(ControlMode.PercentOutput, 0);
+              break;
+            }
+          }
+        }
+      }
+    }
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
   }
